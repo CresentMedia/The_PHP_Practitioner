5240a3661d221d25c20cdaa80dfda5b416194de7
Carlos De Freitas <cdefreitas85@hotmail.co.uk>
2020-11-01 01:26:52 +0000

    Step 7 Assosiatative array completed

---
+++ Step 7 Assosiatative array CD/index.php
@@ -0,0 +1,19 @@
+<?php  
+
+
+
+	$task = [
+
+	"title" => "Complete step 7",
+	"due" => "31-10-20",
+    "assigned_to" => "Carlos",
+    "Completed" => "yes"
+
+];
+
+
+ 
+
+
+require "index.view.php";
+
